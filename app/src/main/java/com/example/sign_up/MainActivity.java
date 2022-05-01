c4011b1a22e79d636aaa566d6202ee98f5591c7f
Majid Fareed <majidtracker15@gmail.com>
2022-05-01 22:27:28 +0500

    added a functionality
    when you click on Enter username, it clears the default text for the user

--- app/src/main/java/com/example/sign_up/MainActivity.java
+++ app/src/main/java/com/example/sign_up/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.sign_up;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+private EditText UserField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UserField = findViewById(R.id.editTextName);
+        UserField.setOnClickListener(view -> {
+            if("Enter username".equals(UserField.getText().toString()))
+            {
+                UserField.setText("");
+            }
+        });
     }
 }
